f9961264e119978c0b9e038b0c524a0605c6d4d4
root <root@localhost.localdomain>
2019-09-12 22:23:05 +0300

    Initial commit

---
+++ src/com/converter/Main.java
@@ -0,0 +1,23 @@
+package com.converter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Input current course: ");
+        Scanner in = new Scanner(System.in);
+        double course = in.nextDouble();
+        System.out.print("Input sum in rub: ");
+        int sumr = in.nextInt();
+
+        System.out.println (convertRuToUsd (sumr, course));
+
+    }
+
+    public static double convertRuToUsd(int sumr, double course) {
+
+        double dollar = sumr * course;
+        return dollar;
+    }
+}
